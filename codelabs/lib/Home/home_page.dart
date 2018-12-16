da6801a5a5de43015967e2168024ab00b6cd4173
LinXunFeng <598600855@qq.com>
2018-12-16 13:53:10 +0800

    使用路由进行界面跳转

--- codelabs/lib/Home/home_page.dart
+++ codelabs/lib/Home/home_page.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'home_cell.dart';
-import 'package:codelabs/TextField/textField_page.dart';
+import 'package:codelabs/Router/router_define_handler.dart';
 
 class Home extends StatelessWidget {
-  final titles = ["TextField"];
-
+  final titles = [
+    "TextField",
+    "Form"
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return new ListView.builder(
       itemCount: titles.length,
       itemBuilder: (context, index) {
         return new FlatButton(
-            child: new HomeCell(),
+            child: new HomeCell(titles[index]),
             onPressed: () {
               print('select index -- $index');
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) {
-                    return new TextFieldPage();
-                  })
-              );
+              final route = RouterDefineHandler.fetchRoute(titles[index]);
+              RouterDefineHandler.router.navigateTo(context, route);
+              // Navigator.push(context,
+              //     MaterialPageRoute(builder: (context) {
+              //       return new TextFieldPage();
+              //     })
+              // );
             });
       },
     );
