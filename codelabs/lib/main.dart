bff9528f3375d1cb719fbcc0cead65f24a311e94
LinXunFeng <598600855@qq.com>
2018-12-16 13:51:38 +0800

    添加初始化路由方法的调用

--- codelabs/lib/main.dart
+++ codelabs/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Home/home_page.dart';
+import 'package:codelabs/Router/router_define_handler.dart';
 
 void main() => runApp(LXFApp());
 
@@ -8,6 +9,9 @@ class LXFApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+    // 初始化路由
+    RouterDefineHandler.init();
+
     return new MaterialApp(
      title: 'Codelabs',
      home: new Scaffold(
