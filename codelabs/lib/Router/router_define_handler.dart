01cf0acf3057f040d98545c151a417d09d548449
LinXunFeng <598600855@qq.com>
2018-12-17 13:16:28 +0800

    Add route

--- codelabs/lib/Router/router_define_handler.dart
+++ codelabs/lib/Router/router_define_handler.dart
@@ -1,6 +1,11 @@
 import 'package:fluro/fluro.dart';
-import 'package:codelabs/TextField/textField_page.dart';
-import 'package:codelabs/Form/form_page.dart';
+import 'package:codelabs/Basic/TextField/textField_page.dart';
+import 'package:codelabs/Basic/Form/form_page.dart';
+import 'package:codelabs/Layout/RowColumn/row_column_page.dart';
+import 'package:codelabs/Layout/Flex/flex_page.dart';
+import 'package:codelabs/Layout/Flow/wrap_page.dart';
+import 'package:codelabs/Layout/Flow/flow_page.dart';
+import 'package:codelabs/Layout/Stack/stack_page.dart';
 
 class RouterDefineHandler {
   static final scheme = 'lxfapp';
@@ -17,6 +22,26 @@ class RouterDefineHandler {
 
     router.define(fetchRoute('Form'), handler:  Handler(handlerFunc: (context, Map<String, dynamic> params) {
       return FormPage();
+    }));
+
+    router.define(fetchRoute('RowColumn'), handler:  Handler(handlerFunc: (context, Map<String, dynamic> params) {
+      return RowColumnPage();
+    }));
+
+    router.define(fetchRoute('FlexExpanded'), handler:  Handler(handlerFunc: (context, Map<String, dynamic> params) {
+      return FlexExpandedPage();
+    }));
+
+    router.define(fetchRoute('Wrap'), handler:  Handler(handlerFunc: (context, Map<String, dynamic> params) {
+      return WrapPage();
+    }));
+
+    router.define(fetchRoute('Flow'), handler:  Handler(handlerFunc: (context, Map<String, dynamic> params) {
+      return FlowPage();
+    }));
+
+    router.define(fetchRoute('Stack'), handler:  Handler(handlerFunc: (context, Map<String, dynamic> params) {
+      return StackPage();
     }));
   }
 
